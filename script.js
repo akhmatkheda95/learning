413ee254fc17cb79a871dca9b35c67a339d63d8d
akhmatkheda95 <khedaakhmathanova@gmail.com>
2024-07-25 13:51:46 +0300

    second task

---
+++ script.js
@@ -0,0 +1,8 @@
+
+        /*let name = prompt('what`s your name?');
+        alert(name)*/
+
+        /*let a = prompt("Первое число?", 1);
+        let b = prompt("Второе число?", 2);
+
+        alert(Number(a) + Number(b));*/
