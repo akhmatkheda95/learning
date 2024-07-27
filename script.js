91dd712a0f78ccd65a188e0f964c48afbf3a904d
akhmatkheda95 <khedaakhmathanova@gmail.com>
2024-07-27 17:13:48 +0300

    third task

--- script.js
+++ script.js
@@ -6,3 +6,11 @@
         let b = prompt("Второе число?", 2);
 
         alert(Number(a) + Number(b));*/
+
+
+        let quest = prompt("Какое официальное название JavaScript?") 
+        if (quest == "ECMAScript") {
+            alert('верно');
+        } else {
+            alert("Не знаете? ECMAScript")
+        }
