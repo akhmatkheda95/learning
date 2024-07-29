1c2a0f35becce91c3537ceebdc7118cd06bcad52
akhmatkheda95 <khedaakhmathanova@gmail.com>
2024-07-29 16:20:20 +0300

    log operators

--- script.js
+++ script.js
@@ -8,9 +8,21 @@
         alert(Number(a) + Number(b));*/
 
 
-        let quest = prompt("Какое официальное название JavaScript?") 
+        /*let quest = prompt("Какое официальное название JavaScript?") 
         if (quest == "ECMAScript") {
             alert('верно');
         } else {
             alert("Не знаете? ECMAScript")
-        }
+        }*/
+
+
+            /*let numb = prompt()
+            if(numb > 0 ) {
+                alert(1)
+            } else if(numb < 0) {
+                alert(-1)
+            } else {
+                alert(0)
+            }*/
+
+                if(!(age >= 14 && age <= 90))
